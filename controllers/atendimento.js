49de9a9051d0632c7e06a1763b12a7721bb7e8d8
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2020-12-06 10:24:53 -0300

    Adição de outros verbos http e finalização da api

--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -1,11 +1,33 @@
+  
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos',(req,res) => res.send("Você está na rota de atendimentos e realizando um GET"))
-    app.post('/atendimentos',(req,res) => {
-        console.log(req.body)
-        const atendimento = req.body
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
 
-        Atendimento.adiciona(atendimento,res)
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscaPorId(id, res)
+    })
+
+    app.post('/atendimentos', (req, res) => {
+       const atendimento = req.body
+
+        Atendimento.adiciona(atendimento, res)
+    }) 
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.deleta(id, res)
     })
 }
--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -1,15 +1,17 @@
 class Tabelas {
     init(conexao) {
         this.conexao = conexao
-        this.criarAtendimento()
+
+        this.criarAtendimentos()
     }
 
-    criarAtendimento() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT,cliente varchar(50) NOT NULL,pet varchar(20),servico varchar(20) NOT NULL,data datetime NOT NULL,dataCriacao datetime NOT NULL, status varchar(20) NOT NULL,observacao text, PRIMARY KEY(id))'
-        this.conexao.query(sql, erro =>{
-            if(erro){
+    criarAtendimentos() {
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+
+        this.conexao.query(sql, erro => {
+            if(erro) {
                 console.log(erro)
-            }else{
+            } else {
                 console.log('Tabela Atendimentos criada com sucesso')
             }
         })
