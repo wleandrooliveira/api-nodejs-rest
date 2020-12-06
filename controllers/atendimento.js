aaf4c2f41b68ce828e62156544801796f1ce6089
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2020-12-06 01:35:56 -0300

    infrasestrutura e configuração de conexão

--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -1,3 +1,7 @@
 module.exports = app => {
-    app.get('/atendimentos',(req,res) => res.send("Você está na rota de atendimentos"))
+    app.get('/atendimentos',(req,res) => res.send("Você está na rota de atendimentos e realizando um GET"))
+    app.post('/atendimentos',(req,res) => {
+        console.log(req.body)
+        res.send("Você está na rota de atendimentos e realizando um POST")
+    })
 }
